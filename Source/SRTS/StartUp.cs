31759ddfa490abb3a110b2190671f5d70aef9a84
Smash Phil <supersmashphil@gmail.com>
2020-01-04 01:56:19 -0700

    bugfix attempt

--- Source/SRTS/StartUp.cs
+++ Source/SRTS/StartUp.cs
@@ -665,7 +665,7 @@ namespace SRTS
         public static void PopulateDictionary()
         {
             srtsDefProjects = new Dictionary<ThingDef, ResearchProjectDef>();
-            List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x.researchPrerequisites?[0].tab.ToString() == "SRTSE").ToList();
+            List<ThingDef> defs = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x?.researchPrerequisites.Count > 0 && x.researchPrerequisites?[0].tab.ToString() == "SRTSE").ToList();
             foreach (ThingDef def in defs)
             {
                 srtsDefProjects.Add(def, def.researchPrerequisites[0]);
